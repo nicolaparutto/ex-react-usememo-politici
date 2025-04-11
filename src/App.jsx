6a714d662ebb2a6339022279c99b195ca96c50f0
nicolaparutto <nicolaparutto2003@gmail.com>
2025-04-11 10:25:46 +0200

    feat: milestone 2 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
 import Card from "./components/Card";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 
 function App() {
 
-  const [politicians, setPoliticians] = useState([])
-
+  const [politicians, setPoliticians] = useState([]);
+  const [wordSearched, setWordSearched] = useState('');
 
   async function fetchPoliticians() {
     try {
       const fetchPromise = fetch("https://boolean-spec-frontend.vercel.app/freetestapi/politicians");
       const fetchData = await fetchPromise;
       const politicianData = await fetchData.json();
-      setPoliticians(politicianData)
+      setPoliticians(politicianData);
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
   }
+
+  const polliticiansFiltered = useMemo(() => {
+    return politicians.filter((politician) => {
+      return politician.name.toLowerCase().includes(wordSearched.toLowerCase()) || politician.biography.toLowerCase().includes(wordSearched.toLowerCase())
+    })
+  })
 
   useEffect(() => {
     fetchPoliticians();
@@ -24,8 +30,17 @@ function App() {
   return (
     <>
       <section className="container">
+        <h1>Lista dei Politici</h1>
+        <input
+          className="search-politician"
+          type="text"
+          placeholder="Cerca per nome o biografia"
+          value={wordSearched}
+          onChange={e => setWordSearched(e.target.value)}
+        />
+
         <div className="cards-container">
-          <Card politicianData={politicians} />
+          <Card politicianData={polliticiansFiltered} />
         </div>
       </section>
     </>
